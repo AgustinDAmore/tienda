0bd3ca7ef05d13ee9131213050df7e6e2068402f
Agustin DAmore <agustindamore50@gmail.com>
2022-07-06 23:20:03 -0300

    b1

--- app/models/entities/Usuario.py
+++ app/models/entities/Usuario.py
@@ -1,5 +1,5 @@
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.security import check_password_hash
 
 class Usuario(UserMixin):
     def __init__(self, id, usuario, password,tipousuario):
